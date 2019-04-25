ab525052d136c6e2db81f063378430646e5503a4
AbdilK <43569235+AbdilK@users.noreply.github.com>
2019-04-25 11:17:04 +0200

    fejl fix

---
+++ src/belman/gui/controller/MainWindowController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belman.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Qash
+ */
+public class MainWindowController implements Initializable {
+
+    @FXML
+    private Button PrevBtn;
+    @FXML
+    private Button NextBtn;
+    @FXML
+    private GridPane gridPaneOneToSix;
+    @FXML
+    private Button btnOne;
+    @FXML
+    private Button btnThree;
+    @FXML
+    private Button btnFive;
+    @FXML
+    private Button btnTwo;
+    @FXML
+    private Button btnFour;
+    @FXML
+    private Button btnSix;
+    @FXML
+    private Button btnSeven;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
